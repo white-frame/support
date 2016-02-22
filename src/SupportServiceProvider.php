3a8a85fb7e60b1c5c7df068319bc869b616c4136
Anael Favre <a.favre@web-6.fr>
2016-02-22 11:59:21 +0100

    Base project

---
+++ src/SupportServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace WhiteFrame\Support;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class SupportServiceProvider
+ * @package WhiteFrame\Support
+ */
+class SupportServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
