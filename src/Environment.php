89dd5e4e072647cda3de6f5e16061de667960487
Anael Favre <anael.favre@gmail.com>
2016-05-12 16:23:58 +0200

    Some tweaks and helper doc

--- src/Environment.php
+++ src/Environment.php
@@ -15,4 +15,24 @@ class Environment
 	{
 		return is_a(app(), 'Illuminate\Foundation\Application');
 	}
+
+	public static function registerAlias($class, $alias)
+	{
+		if(self::isLaravel()) {
+			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+			$loader->alias($class, $alias);
+		}
+		elseif(self::isLumen()) {
+			if(!class_exists($class)) {
+				class_alias($alias, $class);
+			}
+		}
+	}
+
+	public static function registerHelper($name, callable $helper)
+	{
+		app()
+			->make('white-frame.support.helper.manager')
+			->add($name, $helper);
+	}
 }
--- src/Helper/Manager.php
+++ src/Helper/Manager.php
@@ -4,19 +4,44 @@ class Manager
 {
 	protected $helpers = [];
 
+	/**
+	 * Call a new helper with the name and the arguments
+	 *
+	 * @param $method
+	 * @param $parameters
+	 * @return null
+	 */
 	public function __call($method, $parameters) {
 		if(isset($this->helpers[$method]))
-			return call_user_func_array($this->helpers[$method], $parameters);	
+			return call_user_func_array($this->helpers[$method], $parameters);
+		else
+			return null;
 	}
 
+	/**
+	 * Get all helpers registered
+	 *
+	 * @return array
+	 */
 	public function all() {
 		return $this->helpers;
 	}
 
+	/**
+	 * Add a new helper
+	 *
+	 * @param $name
+	 * @param callable $helper
+	 */
 	public function add($name, callable $helper) {
 		$this->helpers[$name] = $helper;
 	}
 
+	/**
+	 * Remove a registered helper
+	 *
+	 * @param $name
+	 */
 	public function remove($name) {
 		unset($this->helpers[$name]);
 	}
